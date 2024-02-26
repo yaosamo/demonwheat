c1d45baf91dfee816245a53eb495a04e137bca91
Yaric <yaosamo@gmail.com>
2024-02-25 22:34:22 -0800

    animation

--- Field.gd
+++ Field.gd
@@ -7,19 +7,27 @@ func _ready():
 	#connect("tools", _on_Tools_emitted())
 	set_process_input(true) # Enable input processing
 	var wheatValue = get_parent().get_node("WheatLabel/WheatValue")
-	
 
-func _on_Tools_emitted():
-	print("Tools signal emitted!")
+
 
 func _input(event):
 	if event is InputEventMouseButton:
 		var mouse_position = event.position
-		if event.pressed and event.button_index == 1:
-# check if mouse if over sprite
+		if event.button_index == 1:
 			if get_rect().has_point(to_local(mouse_position)):
-				print("Manually Harvested")
-				$"/root/Main".wheatValue += 1
+				if event.pressed:
+					print("Manually Harvested")
+					pressed()
+					$"/root/Main".wheatValue += 1
+				elif event.is_released:
+					released()
+					print("released")
+
+func pressed():
+	get_parent().get_node("FieldClick").play("FieldClick", -1, 1.0, false)
+	
+func released():
+	get_parent().get_node("FieldRelease").play("FieldRelease", -1, 1.0, false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- start.gd
+++ start.gd
@@ -5,6 +5,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Button.connect("pressed", pressed.bind())
+	$Button.connect("mouse_entered", hover.bind())
 	pass # Replace with function body.
 
 func pressed():
@@ -12,6 +13,9 @@ func pressed():
 		print("no next scene here")
 	else:
 		next_scene()
+		
+func hover():
+	$AnimationPlayer.play("StartButton")
 		
 func next_scene():
 	var ERR = get_tree().change_scene_to_packed(target_scene)
