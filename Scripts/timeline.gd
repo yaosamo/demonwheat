6e87d17f6d76399395e85beca55927a2f724764d
Yaric <yaosamo@gmail.com>
2024-03-31 21:26:50 -0700

    demon appearance

--- Scripts/timeline.gd
+++ Scripts/timeline.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @export var demonCallTime : float = 12
-var speed : float = 3
+var speed : float = 0.1
 var demonsPresented : bool
 var waveAmount : float = 0
 var maxProgress = 12
+var paid = false
 @export var demonQuota : int = 100
 
 func _ready():
@@ -12,34 +13,62 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Timer
-	demonCallTime -= speed*delta
-	# Progress in percentages just without multiplying by 100
-	var currentProgress = demonCallTime/maxProgress
-	# Maybe add vector point to get it's position as final point
-	$DemonIcon.position.x = lerp(27, 818, currentProgress)
-	$timerLabel.text = str(ceil(demonCallTime), " months til demons")
+	
+	if !demonsPresented:
+		# Timer
+		demonCallTime -= speed*delta
+		# Progress in percentages just without multiplying by 100
+		var currentProgress = demonCallTime/maxProgress
+		# Maybe add vector point to get it's position as final point
+		$DemonIcon.position.x = lerp(27, 818, currentProgress)
+		$timerLabel.text = str(ceil(demonCallTime), " months til demons")
 		
-	if demonCallTime <= 0:
-		demonsPresented = true
-		waveAmount += 1
-		$timerLabel.text = "Demons are here"
+	if demonCallTime <= 0 and !demonsPresented:
+		$timerLabel.text = "Demons are coming"
 		demonCall()
-		# Basically pow function to increase progress
-		maxProgress *= 1.25
-		demonCallTime = maxProgress
-	if demonCallTime <= 1 and !$demonsMus.playing:
-		#$"/root/Main/BgMusic".stop()
+	
+	if demonCallTime <= 10 and !$demonsMus.playing:
+		$"/root/Main/BgMusic".stop()
 		$demonsMus.play()
 		pass
 
 
+
 func demonCall():
-	print_debug("wave#:",waveAmount)
-	$"/root/Main".wheatValue -= demonQuota
-	demonQuota = 100*pow(1.25, waveAmount)
+# add 10s timer if not paid by then goto death
+	$DemAppear.visible = true
+	demonsPresented = true
 	print_debug("hello I am a demon")
+	
+	var lastCallTimer = 0
+	if lastCallTimer <= 0 and !paid:
+		death()
+	
+	
+func death():
+	pass
+
+
+func _on_pay_q_pressed() -> void:
+	$"/root/Main".wheatValue -= demonQuota
+	print_debug("paid")
+	paid = true
 	demonsPresented = false
+	$DemAppear.visible = false
+	$demonsMus.stop()
+	startNextWave()
+	pass # Replace with function body.
+
+
+func startNextWave():
+	# next wave
+	waveAmount += 1
+	print_debug("starting wave#:",waveAmount)
+	# increase quota
+	demonQuota = 100*pow(1.25, waveAmount)
+	# deadline
+	maxProgress *= 1.25
+	demonCallTime = maxProgress
+	# move to starting point
 	$DemonIcon.position.x = maxProgress
-
-
+	$"/root/Main/BgMusic".play()
