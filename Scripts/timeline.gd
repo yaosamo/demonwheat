1fb4084e1cb0373a0330ee6072d43eec5104f42c
Yaric <yaosamo@gmail.com>
2024-04-26 22:03:52 -0700

    fix demons pop-up

--- Scripts/timeline.gd
+++ Scripts/timeline.gd
@@ -5,12 +5,12 @@ extends Node2D
 @onready var game = get_node("/root/GameState")
 
 @export var demonCallTime : float = 12
-var speed : float = 1.25
+var speed : float = 0.25
 var demonsPresented : bool
 var maxProgress = 12
 var paid = false
 var secondTimer = 10
-var baseQuota = 19
+var baseQuota = 199
 var currentProgress
 @export var demonQuota : int = 0
 var demonTalk = preload("res://Scripts/demontalk.tres")
@@ -34,25 +34,24 @@ func _process(delta):
 		# Maybe add vector point to get it's position as final point
 		$DemonIcon.position.x = lerp(27, 818, currentProgress)
 		$timerLabel.text = str(ceil(demonCallTime), " months til demons")
-	
-	#music stop and play when demons arrive	
-	if demonCallTime <= 4 and !$demonsMus.playing:
-		$"/root/Main/BgMusic".stop() 
-		$demonsMus.play()
-		pass
-		
+
+	# music stop and play when demons arrive
+	# if demonCallTime <= 4 and !$demonsMus.playing:
+		#$"/root/Main/BgMusic".stop()
+		#$demonsMus.play()
+		#pass
+
 	if demonCallTime <= 0 and !demonsPresented:
 		$timerLabel.text = "Demons are coming"
 		demonCall()
-		
+
 	if demonsPresented and $DemAppear/ProgressBar.value > 0:
-		#100/second timer gives me step step to achieve desired seconds (5 for example) 100/5 = 20*delta therefore 5 sec to 100 
+		# 100/second timer gives me step step to achieve desired seconds (5 for example) 100/5 = 20*delta therefore 5 sec to 100 
 		$DemAppear/ProgressBar.value -= (100/secondTimer)*delta
 		$DemAppear/lastTimerLabel.text = "Give up in.." + str(ceil($DemAppear/ProgressBar.value/(100/secondTimer)))
-		
+
 	if $DemAppear/ProgressBar.value < 1 and !paid:
 		death()
-
 
 
 func demonCall():
@@ -65,6 +64,7 @@ func demonCall():
 	$DemAppear.visible = true
 	demonsPresented = true
 	print_debug("hello I am a demon")
+	pass
 	
 	
 func death():
@@ -90,9 +90,10 @@ func _on_pay_q_pressed() -> void:
 		game.wheat -= demonQuota
 		print_debug("paid")
 		paid = true
-		demonsPresented = false 
+		demonsPresented = false
 		$DemAppear/ProgressBar.value = 100
 		$DemAppear.visible = false
+		print_debug("Demons are invisible")
 		$demonsMus.stop()
 		startNextWave()
 	pass 
@@ -107,6 +108,7 @@ func startNextWave():
 	demonQuota = calcQuota()
 	$quotaValueDisplay.text = "Demons quota: %d" % demonQuota
 	paid = false
+	demonCallTime = maxProgress
 	# move to starting point
 	$DemonIcon.position.x = maxProgress
 	$"/root/Main/BgMusic".play()
@@ -114,4 +116,3 @@ func startNextWave():
 
 func calcQuota() -> int:
 	return baseQuota*pow(1.25, game.wave)
-	
