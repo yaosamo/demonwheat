f0c2599ddbf4291273c8d44387d238caa22714b8
Yaric <yaosamo@gmail.com>
2024-04-28 01:01:04 -0700

    refactoring, adding containers

---
+++ Scripts/bonusess.gd
@@ -0,0 +1,24 @@
+extends Button
+
+
+@export var bonusname: String
+@export var bonusdesc: String
+@export var price: int
+@export var amount : int
+@export var baseprice : int
+@export var buttonH : int
+@export var buttonW : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.size = Vector2(buttonW, buttonH)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Title.text = bonusname
+	$PriceValue.text = str(price)
+	$Amount.text = str(amount)
+	$Desc.text = str(bonusdesc)
+	pass
