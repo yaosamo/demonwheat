ae63a84a9afb7a398cf23e18ee6fabf18c4dcdec
Yaric <yaosamo@gmail.com>
2024-05-26 21:55:02 -0700

    unitbuilding - particles

--- Scripts/unitbuilding.gd
+++ Scripts/unitbuilding.gd
@@ -13,20 +13,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var pos_goal = get_parent().get_node("unit_br_goal").position
-	var pos_start = get_parent().get_node("unit_br_start").position
-
-	timer -= speed*delta
-	progress = timer/maxposition
-
-	#$unit.position.x = lerp(27, pos_start.x, progress)
-	#if $unit.position.x > pos_start.x:
-	$unit.position.x = lerp(574, int(pos_start.x), progress)
-	#$unit.position.y = lerp(int(pos_goal.y_, int(pos_start.y), progress)
 	
-	#$unit.global_position.y = lerp(10, 20, progress)
-	
-	print_debug("Unit pos ", $unit.global_position)
 	pass
 
 
